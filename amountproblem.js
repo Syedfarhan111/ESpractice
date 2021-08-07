82773a52943f7a7445767906306932d98b5e1dfa
Syedfarhan111 <syedfarhan5170@gmail.com>
2021-08-07 17:13:08 +0530

    hh

--- amountproblem.js
+++ amountproblem.js
@@ -1,4 +1,3 @@
-const millegeCost=( dist ,mileage , fuelprice) => (dist/mileage)* fuelprice;
+const millegeCost = (dist, mileage, fuelprice) => (dist / mileage) * fuelprice;
 
-
-    console.log(`amount neet to full fuel it${millegeCost( 300,50,100)}`);
+console.log(`amount neet to full fuel it${millegeCost(300, 50, 100)}`);
