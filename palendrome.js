5931d0267dc57c26ad9ccc75386d81ffa54d1434
Syedfarhan111 <syedfarhan5170@gmail.com>
2021-08-10 16:18:36 +0530

    palendrom

---
+++ palendrome.js
@@ -0,0 +1,13 @@
+// const palendrome = (str) => {
+//   const arrstr = str.split("");
+//   console.log(arrstr);
+//   console.log(arrstr.join());
+// };
+
+// palendrome("madam");
+
+const palendromeOptm = (str) => {
+  return str === str.split("").reverse().join(o"");
+};
+
+console.log(palendromeOptm("7870000"));
